02ad48b942c5da6bfe5c11bda2bbed9bca861507
Nick Landas <nick.landas@gmail.com>
2022-12-15 11:46:25 -0600

    commit tree 21ded7d6f271aeda14f2240ae9ee3893903d1b5f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet (array)
+  new_array = array.map{|arr| "#{arr.capitalize}!"}
+  p new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+  p array.any? {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese (array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  found = array.find {|cheese| cheese_types.include?(cheese)}
+  p found
 end
+
+roll_call_dwarves(["happy", "sad", "bashful", "dopey"])
+summon_captain_planet(["happy", "sad", "bashful", "dopey"])
+long_planeteer_calls(["come", "cry", "jump"])
+long_planeteer_calls(["come", "thrash", "fly", "go"])
+find_the_cheese(["triscuit","ritz", "gouda", "pepperoni"])
+find_the_cheese(["carrot", "broccoli", "celery"])
